4e10442f61d36703cbc52a958e6b3b2e9b37cc64
Su Anli <su.anli@anlitz.com>
2024-02-13 19:40:45 +0800

    feat(shared/ui): add box button

--- apps/board-game-app/src/pages/welcome-page/welcome-page.tsx
+++ apps/board-game-app/src/pages/welcome-page/welcome-page.tsx
@@ -1,11 +1,15 @@
+import { BoxButton } from '@shared/ui';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const WelcomePage = () => {
   return (
-    <View>
-      <SafeAreaView>
-        <Text>Welcome Page</Text>
+    <View style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Text>Welcome Page</Text>
+        </View>
+        <BoxButton title="Login" />
       </SafeAreaView>
     </View>
   );
