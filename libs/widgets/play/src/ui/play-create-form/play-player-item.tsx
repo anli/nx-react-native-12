f8931080dd1ba0a3ed8882b75a9ece73424ed508
Su Anli <anli@users.noreply.github.com>
2024-03-30 13:47:52 +0800

    feat: add remove player to PlayCreateForm (#25)

--- libs/widgets/play/src/ui/play-create-form/play-player-item.tsx
+++ libs/widgets/play/src/ui/play-create-form/play-player-item.tsx
@@ -1,27 +1,31 @@
-import { Icon, List } from '@shared/ui';
+import { ActionChip, Icon, List } from '@shared/ui';
 import { FC } from 'react';
 import { Pressable } from 'react-native';
 
 type PlayPlayerItemProps = {
+  id: string;
   name: string;
   score?: number;
   isWinner?: boolean;
+  onWinnerPress?: () => void;
+  onDelete?: () => void;
 };
 
 export const PlayPlayerItem: FC<PlayPlayerItemProps> = ({
   name = '',
-  score,
-  isWinner,
+  score = 0,
+  isWinner = false,
+  onWinnerPress,
+  onDelete,
 }) => {
-  const description =
-    score !== undefined ? `${score} points` : 'Tap to update result';
-
   return (
     <List.Item
       title={name}
-      description={description}
       LeftComponent={
-        <Pressable className="items-center justify-center">
+        <Pressable
+          className="items-center justify-center"
+          onPress={onWinnerPress}
+        >
           {isWinner ? (
             <Icon name="TrophyIcon" size={20} color="black" type="outline" />
           ) : (
@@ -30,14 +34,17 @@ export const PlayPlayerItem: FC<PlayPlayerItemProps> = ({
         </Pressable>
       }
       RightComponent={
-        <Pressable>
-          <Icon
-            name="EllipsisVerticalIcon"
-            size={24}
-            color="gray"
-            type="outline"
-          />
-        </Pressable>
+        <>
+          <ActionChip title={`${score}`} small outlined />
+          <Pressable>
+            <Icon
+              name="TrashIcon"
+              color="gray"
+              type="outline"
+              onPress={onDelete}
+            />
+          </Pressable>
+        </>
       }
     />
   );
