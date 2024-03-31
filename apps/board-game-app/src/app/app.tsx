ffe2d1fd3f10b74f96a083f950b0586f05c554a3
Su Anli <su.anli@anlitz.com>
2024-03-31 14:07:37 +0800

    chore: add @gorhom/bottom-sheet lib

--- apps/board-game-app/src/app/app.tsx
+++ apps/board-game-app/src/app/app.tsx
@@ -3,6 +3,7 @@ import { AuthenticationProvider, QueryClientProvider } from '@shared/utils';
 import { remapProps } from 'nativewind';
 import { FlatList } from 'react-native';
 import Config from 'react-native-config';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import './../../global.css';
 import { RootStack } from './navigation';
@@ -15,17 +16,19 @@ remapProps(FlatList, {
 
 export const App = () => {
   return (
-    <SafeAreaProvider>
-      <AuthenticationProvider
-        domain={Config.AUTH0_DOMAIN}
-        clientId={Config.AUTH0_CLIENT_ID}
-      >
-        <QueryClientProvider>
-          <NavigationContainer>
-            <RootStack />
-          </NavigationContainer>
-        </QueryClientProvider>
-      </AuthenticationProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView className="flex-1">
+      <SafeAreaProvider>
+        <AuthenticationProvider
+          domain={Config.AUTH0_DOMAIN}
+          clientId={Config.AUTH0_CLIENT_ID}
+        >
+          <QueryClientProvider>
+            <NavigationContainer>
+              <RootStack />
+            </NavigationContainer>
+          </QueryClientProvider>
+        </AuthenticationProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 };
